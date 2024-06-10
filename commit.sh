ccecaf46fc3c0823b6819cf08e88b4b70e30c9c3
ErnestoAlfonso <ernealfonso00@gmail.com>
2024-06-10 18:56:10 -0400

    New prove with a diferent main.yml

--- commit.sh
+++ commit.sh
@@ -1,22 +1,22 @@
-AZUREPAT=$AZUREPAT
-AZUSERNAME=$AZUSERNAME
-AZUSER_EMAIL=$AZUSER_EMAIL
-AZORG=$AZORG
-git clone https://github.com/ErnestoAlfonso/GameStore
-cd GameStore
-rm -rf .git
+# AZUREPAT=$AZUREPAT
+# AZUSERNAME=$AZUSERNAME
+# AZUSER_EMAIL=$AZUSER_EMAIL
+# AZORG=$AZORG
+# git clone https://github.com/ErnestoAlfonso/GameStore
+# cd GameStore
+# rm -rf .git
 
-cd ..
+# cd ..
 
-GIT_CMD_REPOSITORY="https://ernealfons00@gmail.com:$AZUREPAT@dev.azure.com/ernealfonso00/Prueba1/_git/Prueba1"
-git clone $GIT_CMD_REPOSITORY
+# GIT_CMD_REPOSITORY="https://ernealfons00@gmail.com:$AZUREPAT@dev.azure.com/ernealfonso00/Prueba1/_git/Prueba1"
+# git clone $GIT_CMD_REPOSITORY
 
-cp -r Prueba1/Repos/Files/Prueba1/
+# cp -r Prueba1/Repos/Files/Prueba1/
 
-git config --global user.email "ernealfonso00@gmail.com"
-git config --global user.name "ernealfonso00@gmail.com"
+# git config --global user.email "ernealfonso00@gmail.com"
+# git config --global user.name "ernealfonso00@gmail.com"
 
-git add .
-git commit -m "sync from git to azure"
+# git add .
+# git commit -m "sync from git to azure"
 
-git push
+# git push
