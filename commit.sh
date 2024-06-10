5b3a0111bedce8e763f05bfbc540aec118f4d15f
ErnestoAlfonso <ernealfonso00@gmail.com>
2024-06-10 18:38:53 -0400

    q

--- commit.sh
+++ commit.sh
@@ -8,15 +8,15 @@ rm -rf .git
 
 cd ..
 
-GIT_CMD_REPOSITORY="https://$AZUSERNAME:$AZUREPAT@dev.azure.com/$AZORG/Prueba1/_git/Prueba1"
+GIT_CMD_REPOSITORY="https://ernealfons00@gmail.com:$AZUREPAT@dev.azure.com/$AZORG/Prueba1/_git/Prueba1"
 git clone $GIT_CMD_REPOSITORY
 
-cp -r Prueba1/* Prueba1/
+cp -r Prueba1/Repos/Files/Prueba1/
 
 cd Prueba1
 
-git config --global user.email "$AZUSER_EMAIL"
-git config --global user.name "$AZUSERNAME"
+git config --global user.email "ernealfonso00@gmail.com"
+git config --global user.name "ernealfonso00@gmail.com"
 
 git add .
 git commit -m "sync from git to azure"
